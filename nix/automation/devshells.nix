17b711afb497110559a14f5e01a82a4f99a3c2f5
Joshua Gilman <joshuagilman@gmail.com>
2022-11-03 15:50:42 -0700

    build: adds tooling configs

--- nix/automation/devshells.nix
+++ nix/automation/devshells.nix
@@ -1,17 +1,23 @@
-{ inputs
-, cell
-}:
-let
+{
+  inputs,
+  cell,
+}: let
   inherit (inputs) nixpkgs std;
   l = nixpkgs.lib // builtins;
 in
-l.mapAttrs (_: std.lib.dev.mkShell) {
-  default = { ... }: {
-    name = "lab devshell";
-    imports = [ std.std.devshellProfiles.default ];
-    packages = [
-      nixpkgs.ansible
-      nixpkgs.terraform
-    ];
-  };
-}
+  l.mapAttrs (_: std.lib.dev.mkShell) {
+    default = {...}: {
+      name = "lab devshell";
+      imports = [std.std.devshellProfiles.default];
+      nixago = [
+        cell.configs.conform
+        cell.configs.lefthook
+        cell.configs.prettier
+        cell.configs.treefmt
+      ];
+      packages = [
+        nixpkgs.ansible
+        nixpkgs.terraform
+      ];
+    };
+  }
