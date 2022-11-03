93f1ecab4d69cc4034ab3622cfd872384ccecb4e
Joshua Gilman <joshuagilman@gmail.com>
2022-11-03 16:33:33 -0700

    feat: adds command for initializing LXD

--- nix/automation/tasks.nix
+++ nix/automation/tasks.nix
@@ -5,7 +5,19 @@
   inherit (inputs) nixpkgs std;
   l = nixpkgs.lib // builtins;
 in {
-  init = nixpkgs.writeShellScriptBin "init" ''
-    echo "Initializing..."
-  '';
+  init = nixpkgs.writeShellApplication {
+    name = "init";
+    text = ''
+      # This is a destructive command, so double-check...
+      read -p "Are you sure you want to intiailize LXD? " -n 1 -r
+      echo
+      if [[ ! $REPLY =~ ^[Yy]$ ]]
+      then
+          exit 1
+      fi
+
+      # Initialize LXD
+      lxd init --preseed "${inputs.self + "/lxd/conf.yaml"}"
+    '';
+  };
 }
