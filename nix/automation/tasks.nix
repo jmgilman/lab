4e534798378f6aa34cda22c826f4e909d0c7893a
Joshua Gilman <joshuagilman@gmail.com>
2022-11-05 11:47:45 -0700

    build: improves initialization logic

--- nix/automation/tasks.nix
+++ nix/automation/tasks.nix
@@ -8,16 +8,26 @@ in {
   init = nixpkgs.writeShellApplication {
     name = "init";
     text = ''
-      # This is a destructive command, so double-check...
-      read -p "Are you sure you want to intiailize LXD? " -n 1 -r
-      echo
-      if [[ ! $REPLY =~ ^[Yy]$ ]]
-      then
-          exit 1
+      # Check that LXD is available
+      if ! command -v lxd &> /dev/null; then
+        echo "LXD must be enabled before running initialization"
+        echo "Please configure the appropriate modules on NixOS first"
+        exit 1
+      else
+        echo "LXD found..."
       fi
 
-      # Initialize LXD
-      lxd init --preseed "${inputs.self + "/lxd/conf.yaml"}"
+      # Attempt to discern if it's been initialized
+      pools=$(lxc storage list -f json | ${nixpkgs.jq}/bin/jq ". | length")
+      if [[ $pools -lt 1 ]]; then
+        echo "LXD appears to be uninitialized."
+        echo "Initializing..."
+        lxd init --preseed "${inputs.self + "/lxd/conf.yaml"}"
+      else
+        echo "LXD is initialized..."
+      fi
+
+      echo "Initialization complete!"
     '';
   };
 }
